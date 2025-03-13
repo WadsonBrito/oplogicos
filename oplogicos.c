77dbf85e59b8b011788f57d37fb6ba879fbef88b
WadsonBrito <wadsonmelo@gmail.com>
2025-03-13 18:19:30 +0000

    oplogicos idade altura

--- oplogicos.c
+++ oplogicos.c
@@ -1,1 +1,18 @@
+# include <stdio.h>
+int main(){
+    int idade;
+    float altura;
+
+    printf(" Infome sua idade: \n");
+    scanf("%d", &idade);
+    printf("informe sua altura: \n");
+    scanf("%f", &altura);
+
+    if(idade >= 18 && idade <= 30 && altura > 1.70){
+        printf("Parabéns você atende aos critérios!!\n");
+    } else {
+        printf("Infelizmente você não atende aos critérios\n");
+    }
+    return 0;
+}
 
